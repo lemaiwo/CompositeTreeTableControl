022fe96b55215dda707ea79bb66255d076536f89
Wouter Lemaire <wouter.lemaire@flexso.com>
2020-11-01 19:55:42 +0100

    added more functionalities

--- webapp/control/TreeWithButtons.js
+++ webapp/control/TreeWithButtons.js
@@ -2,6 +2,10 @@ sap.ui.define(['sap/ui/core/XMLComposite'], function (XMLComposite) {
 	"use strict";
 	var TreeWithButtons = XMLComposite.extend("be.wl.CompositeControlExample.control.TreeWithButtons", {
 		metadata: {
+			properties: {
+				rowCount: { type: 'int', defaultValue: 20, visibility: 'hidden' },
+				editable: 'boolean'
+			},
 			aggregations: {
 				columns: {
 					type: "sap.ui.table.Column",
@@ -18,29 +22,176 @@ sap.ui.define(['sap/ui/core/XMLComposite'], function (XMLComposite) {
 					// the aggregation will be forwarded directly to the corresponding aggregation in the inner table in table.control.xml
 					forwarding: {
 						idSuffix: "--innerTreeTable",
-						aggregation: "rows"
+						aggregation: "rows",
+						forwardBinding: true
+					}
+				}
+			},
+			events: {
+				add: {
+					parameters: {
+						selectedIndex: { type: 'integer' },
+						selectedPath: { type: 'string' },
+						selectedContext: { type: 'sap.ui.model.Context' }
+					}
+				},
+				delete: {
+					parameters: {
+						selectedIndex: { type: 'integer' },
+						selectedPath: { type: 'string' },
+						selectedContext: { type: 'sap.ui.model.Context' }
+					}
+				},
+				move: {
+					parameters: {
+						draggedRowContexts: { type: 'sap.ui.model.Context' },
+						draggedRowIndex: { type: 'integer' },
+						newTargetContext: { type: 'sap.ui.model.Context' }
 					}
 				}
 			}
 		},
+		init:function(){
+		},
+		onAfterRendering: function () {
+			this.setRowCount(this.calculateRows());
+			this.getBinding("rows").attachChange(function(){
+				this.setRowCount(this.calculateRows());
+			},this);
+		},
+		updateRows: function (o) {
+			var oTreeTable = this.byId("innerTreeTable");
+			return oTreeTable.prototype.updateRows.call(oTreeTable, o);
+
+		},
+		calculateRows: function () {
+			var rows = this.getBinding("rows");
+			return this.countRows(rows.getNodes(0).filter(function (node) {
+				return node.level === 0;
+			}))
+		},
+		countRows: function (nodes) {
+			var oTreeTable = this.byId("innerTreeTable");
+			var rows = this.getBinding("rows");
+
+			return nodes.reduce(function (count, node) {
+				count += 1;
+				if (node.nodeState.expanded) {
+					count += this.countRows(node.children);
+				}
+				return count;
+			}.bind(this), 0);
+		},
+		getRowModel: function () {
+			var rowBindingInfo = this.getBindingInfo("rows");
+			return this.getModel(rowBindingInfo.model);
+		},
+		getRowBindingContexts: function () {
+			var rowBindingInfo = this.getBindingInfo("rows");
+			return this.getRowModel().getProperty(rowBindingInfo.binding.getPath());
+		},
 		onCollapseAll: function () {
 			var oTreeTable = this.byId("innerTreeTable");
 			oTreeTable.collapseAll();
+			this.updateVisibleRowCount();
 		},
 
 		onCollapseSelection: function () {
 			var oTreeTable = this.byId("innerTreeTable");
-			oTreeTable.collapse(oTreeTable.getSelectedIndices());
+			if (oTreeTable.getSelectedIndex() > -1) {
+				oTreeTable.collapse(oTreeTable.getSelectedIndices());
+				this.updateVisibleRowCount();
+			}
 		},
 
 		onExpandFirstLevel: function () {
 			var oTreeTable = this.byId("innerTreeTable");
 			oTreeTable.expandToLevel(1);
+
+			this.updateVisibleRowCount();
 		},
 
 		onExpandSelection: function () {
 			var oTreeTable = this.byId("innerTreeTable");
-			oTreeTable.expand(oTreeTable.getSelectedIndices());
+			if (oTreeTable.getSelectedIndex() > -1) {
+				oTreeTable.expand(oTreeTable.getSelectedIndices());
+				this.updateVisibleRowCount();
+			}
+		},
+		onToggleRow: function (oEvent) {
+			this.updateVisibleRowCount();
+		},
+		getArrayNames: function () {
+			var rowBindingInfo = this.getBindingInfo("rows");
+			return rowBindingInfo.parameters.arrayNames[0];
+
+		},
+		getSelection: function () {
+			var selectedIndex = this.byId("innerTreeTable").getSelectedIndex(),
+				rowsModel = this.getModel(this.getBindingInfo("rows").model),
+				oTreeTable = this.byId("innerTreeTable");
+			if (selectedIndex > -1) {
+				var selectedRowContext = oTreeTable.getContextByIndex(selectedIndex),
+					path = selectedRowContext.getPath() + "/" + this.getArrayNames();
+			} else {//add to root in case nothing is selected
+				path = this.getBindingInfo("rows").path;
+			}
+			return {
+				selectedIndex: selectedIndex,
+				selectedPath: path,
+				selectedContext: selectedRowContext
+			};
+		},
+		onAdd: function (oEvent) {
+			var oTreeTable = this.byId("innerTreeTable");
+			var selection = this.getSelection();
+			this.fireAdd(selection);
+			oTreeTable.expand((selection.selectedIndex || 0));
+			// this.updateVisibleRowCount();
+		},
+		onDelete: function () {
+			this.fireDelete(this.getSelection());
+			// this.updateVisibleRowCount();
+		},
+		onDragStart: function (oEvent) {
+			var oTreeTable = this.byId("innerTreeTable");
+			var oDragSession = oEvent.getParameter("dragSession");
+			var oDraggedRow = oEvent.getParameter("target");
+			var iDraggedRowIndex = oDraggedRow.getIndex();
+			var aDraggedRowContexts = [];
+			var oDraggedRowContext = oTreeTable.getContextByIndex(iDraggedRowIndex);
+			aDraggedRowContexts.push(oDraggedRowContext);
+
+			oDragSession.setComplexData("tree", {
+				draggedRowContexts: aDraggedRowContexts,
+				draggedRowIndex: iDraggedRowIndex
+			});
+		},
+
+		onDrop: function (oEvent) {
+			var oTreeTable = this.byId("innerTreeTable");
+			var oDragSession = oEvent.getParameter("dragSession");
+			var oDroppedRow = oEvent.getParameter("droppedControl");
+			if (!oDragSession.getComplexData("tree")) {
+				return;
+			}
+			var aDraggedRowContexts = oDragSession.getComplexData("tree").draggedRowContexts;
+			var draggedRowIndex = oDragSession.getComplexData("tree").draggedRowIndex;
+			var oNewTargetContext = oTreeTable.getContextByIndex(oDroppedRow.getIndex());
+
+			this.fireMove({
+				draggedRowContexts: aDraggedRowContexts,
+				draggedRowIndex: draggedRowIndex,
+				newTargetContext: oNewTargetContext
+			});
+
+			// this.updateVisibleRowCount();
+		},
+		updateVisibleRowCount: function () {
+			this.setRowCount(this.calculateRows());
+		},
+		setRowCount: function (count) {
+			this.setProperty("rowCount", count);
 		}
 	});
 	return TreeWithButtons;
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -8,6 +8,67 @@ sap.ui.define([
 		onInit: function () {
 			var oModel = new JSONModel("./model/Clothing.json");
 			this.getView().setModel(oModel);
+		},
+		onAddLine: function (oEvent) {
+			var treeModel = this.getView().getModel();
+			var path = oEvent.getParameter("selectedPath");
+			
+			var rowData = treeModel.getProperty(path) || [];
+			rowData.push({});
+			treeModel.setProperty(path, rowData);
+		},
+		onDeleteLine: function (oEvent) {
+			var selectedRowContext = oEvent.getParameter("selectedContext");
+			if (selectedRowContext) {
+				this._removeRow(selectedRowContext);
+			}
+		},
+		_removeRow: function (RemoveRowContext) {
+			var treeModel = this.getView().getModel();
+			var path = RemoveRowContext.getPath().split("/");
+			var lastone = parseInt(path[path.length - 1], 10);
+			path.pop();
+			path = path.join("/");
+			var updateDraggedParentItems = treeModel.getProperty(path).filter(function (item, index) {
+				return index !== lastone;
+			});
+			treeModel.setProperty(path, updateDraggedParentItems);
+		},
+		onMoveRow: function (oEvent) {
+			var aDraggedRowContexts = oEvent.getParameter("draggedRowContexts");
+			var draggedRowIndex = oEvent.getParameter("draggedRowIndex");
+			var oNewTargetContext = oEvent.getParameter("newTargetContext");
+			var treeModel = this.getView().getModel();
+
+			if (aDraggedRowContexts.length === 0 || !oNewTargetContext) {
+				return;
+			}
+
+			var oNewTarget = oNewTargetContext.getProperty();
+
+			for (var i = 0; i < aDraggedRowContexts.length; i++) {
+				if (oNewTargetContext.getPath().indexOf(aDraggedRowContexts[i].getPath()) === 0) {
+					// Avoid moving a node into one of its child nodes.
+					continue;
+				}
+				var targetPath = oNewTargetContext.getPath().split("/");
+				targetPath.pop();
+				targetPath = targetPath.join("/");
+				var updateTargetItems = treeModel.getProperty(targetPath).reduce(function (items, item, index) {	
+					//add existing line unless it is the dragged row that was already on the same level				
+					if (item !== aDraggedRowContexts[i].getProperty()) {
+						items.push(item);
+					}
+					//add dragged row after the place it has been dropped
+					if (item === oNewTargetContext.getObject()) {
+						items.push(aDraggedRowContexts[i].getProperty());
+					}
+					return items;
+				}, []);
+
+				this._removeRow(aDraggedRowContexts[i]);
+				treeModel.setProperty(targetPath, updateTargetItems);
+			}
 		}
 	});
 });
